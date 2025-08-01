196b74d900ef57463ce0d3697f874e6d01eb0e50
seonmin8284 <seonmin8284@naver.com>
2025-08-01 20:49:42 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Dash_carbon_dashboard2/',
   plugins: [react()],
   server: {
     port: 3000,
